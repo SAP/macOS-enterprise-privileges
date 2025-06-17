d0075d3e5b1156d9df2b8042de61d31a12fca494
Marc Thielemann <marc.thielemann@me.com>
2025-06-17 10:46:23 +0200

    Add files via upload

--- application_management/Privileges2_Uninstall.sh
+++ application_management/Privileges2_Uninstall.sh
@@ -25,31 +25,31 @@ currentUser=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
 
 if [[ -n "$currentUser" && "$currentUser" != "root" ]]; then
 
-	# make sure the current user has admin rights after uninstallation
-	isNotAdmin=$(/usr/bin/dsmemberutil checkmembership -U "$currentUser" -G admin | /usr/bin/grep -i "is not")
+   # make sure the current user has admin rights after uninstallation
+   isNotAdmin=$(/usr/bin/dsmemberutil checkmembership -U "$currentUser" -G admin | /usr/bin/grep -i "is not")
 
-	if [[ -n "$isNotAdmin" ]]; then
-		/usr/sbin/dseditgroup -o edit -a "$currentUser" -t user admin
-	fi
-	
-	# unload the launch agent and quit all of our applications
-	/bin/launchctl bootout gui/$(/usr/bin/id -u "$currentUser") /Library/LaunchAgents/corp.sap.privileges.agent.plist
-	/bin/sleep 2 && /usr/bin/sudo -u "$currentUser" /usr/bin/killall "Privileges" "PrivilegesAgent" "PrivilegesCLI"
-		
-	# delete user-specific files
-	userHome=$(/usr/bin/dscl . -read "/Users/$currentUser" NFSHomeDirectory | /usr/bin/sed 's/^[^\/]*//g')
-	
-	if [[ -d "$userHome" && "$userHome" != "/var/empty" ]]; then
-	
-		/usr/bin/sudo -u "$currentUser" /usr/bin/defaults delete corp.sap.privileges
-		/usr/bin/sudo -u "$currentUser" /usr/bin/defaults delete corp.sap.privileges.agent
-		/usr/bin/sudo -u "$currentUser" /usr/bin/defaults delete corp.sap.privileges.docktileplugin
-		
-		/bin/rm -rf "${userHome}/Library/Preferences/corp.sap.privileges"* \
-					"${userHome}/Library/Containers/corp.sap.privileges"* \
-					"${userHome}/Library/Group Containers/7R5ZEU67FQ.corp.sap.privileges" \
-					"${userHome}/Library/Application Scripts/corp.sap.privileges"*
-	fi
+   if [[ -n "$isNotAdmin" ]]; then
+      /usr/sbin/dseditgroup -o edit -a "$currentUser" -t user admin
+   fi
+   
+   # unload the launch agent and quit all of our applications
+   /bin/launchctl bootout gui/$(/usr/bin/id -u "$currentUser") /Library/LaunchAgents/corp.sap.privileges.agent.plist
+   /bin/sleep 2 && /usr/bin/sudo -u "$currentUser" /usr/bin/killall "Privileges" "PrivilegesAgent" "PrivilegesCLI"
+      
+   # delete user-specific files
+   userHome=$(/usr/bin/dscl . -read "/Users/$currentUser" NFSHomeDirectory | /usr/bin/sed 's/^[^\/]*//g')
+   
+   if [[ -d "$userHome" && "$userHome" != "/var/empty" ]]; then
+   
+      /usr/bin/sudo -u "$currentUser" /usr/bin/defaults delete corp.sap.privileges
+      /usr/bin/sudo -u "$currentUser" /usr/bin/defaults delete corp.sap.privileges.agent
+      /usr/bin/sudo -u "$currentUser" /usr/bin/defaults delete corp.sap.privileges.docktileplugin
+      
+      /bin/rm -rf "${userHome}/Library/Preferences/corp.sap.privileges"* \
+                  "${userHome}/Library/Containers/corp.sap.privileges"* \
+                  "${userHome}/Library/Group Containers/7R5ZEU67FQ.corp.sap.privileges" \
+                  "${userHome}/Library/Application Scripts/corp.sap.privileges"*
+   fi
 fi
 
 # unload the launch daemons and quit them
@@ -59,12 +59,12 @@ fi
 
 # remove the global stuff
 /bin/rm -rf "/Library/LaunchDaemons/corp.sap.privileges"* \
-			"/Library/LaunchAgents/corp.sap.privileges"* \
-			"/Library/Application Support/Privileges" \
-			"/Applications/Privileges.app" \
-			"/private/etc/paths.d/PrivilegesCLI" \
-			"/Library/Scripts/VoiceOver/Privileges Time Left.scpt" \
-			"/Library/Application Support/JAMF/Receipts/Privileges_"*.pkg
+            "/Library/LaunchAgents/corp.sap.privileges"* \
+            "/Library/Application Support/Privileges" \
+            "/Applications/Privileges.app" \
+            "/private/etc/paths.d/PrivilegesCLI" \
+            "/Library/Scripts/VoiceOver/Privileges Time Left.scpt" \
+            "/Library/Application Support/JAMF/Receipts/Privileges_"*.pkg
 
 # remove the package receipt
 /usr/sbin/pkgutil --forget "corp.sap.privileges.pkg"
